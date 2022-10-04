97bd7cc4a0f38b83a5ced974795c09e591dc1567
Troll0ton <anton.shulov12345@gmail.com>
2022-10-04 15:19:13 +0300

    added something

--- stack.cpp
+++ stack.cpp
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <locale.h>
+
+//-----------------------------------------------------------------------------
+
+//#define DUMP(varname) #varname
+
+//-----------------------------------------------------------------------------
+
+//#define POISON 50161703917
 
 //-----------------------------------------------------------------------------
 
@@ -11,57 +20,65 @@
 
 //-----------------------------------------------------------------------------
 
-int stack_Ctor   (struct Stack *stk, int capacity_ctor);
-int stack_Dtor   (struct Stack *stk);
+int stack_ctor   (struct Stack *stk, int capacity_ctor);
+int stack_dtor   (struct Stack *stk);
 
-int stack_pop    (struct Stack *stk);
+double stack_pop (struct Stack *stk);
 int stack_push   (struct Stack *stk, double elem);
 int stack_resize (struct Stack *stk, int opt_resize);
+
+int stack_dmp    (struct Stack *stk);
 
 //-----------------------------------------------------------------------------
 
 struct Stack
 {
-    double *data_stk;
+    char   *name_stk;
+    double *buffer_stk;
 
-    int capacity_stk;
-    int size_stk;
+    int     capacity_stk;
+    int     size_stk;
 };
 
 //-----------------------------------------------------------------------------
 
 int main ()
 {
-    struct Stack stk1 = {NULL, 0, 0};
-    stack_Ctor (&stk1, 10);// == 0 || Error ("");
+    setlocale (LC_ALL, "Russian");
 
-    stack_push (&stk1, 100);
-    stack_push (&stk1, 101);
-    stack_push (&stk1, 102);
-    stack_push (&stk1, 103);
+    struct Stack stk1 = {0};
 
-    stack_pop (&stk1);
+    stack_ctor (&stk1, 10);// == 0 || Error ("");
 
-    stack_Dtor (&stk1);
+    for(int i = 1000; i < 1056; i++)
+    {
+        stack_push (&stk1, i);
+    }
+
+    stack_pop  (&stk1);
+
+    stack_dmp  (&stk1);
+
+    stack_dtor (&stk1);
 
     return 0;
 }
 
 //-----------------------------------------------------------------------------
 
-int stack_Ctor (struct Stack *stk, int capacity_ctor)
+int stack_ctor (struct Stack *stk, int capacity_ctor)
 {
-    (stk -> capacity_stk) = capacity_ctor;
-    (stk -> size_stk) = 0;
+    stk->capacity_stk = capacity_ctor;
+    stk->size_stk = 0;
 
-    (stk -> data_stk) = (double*) calloc (capacity_ctor, sizeof (double));
+    stk->buffer_stk = (double*) calloc (1, capacity_ctor * sizeof (double));
 
     return SUCCESS;
 }
 
 //-----------------------------------------------------------------------------
 
-int stack_Dtor (struct Stack *stk)
+int stack_dtor (struct Stack *stk)
 {
     free (stk);
 
@@ -72,14 +89,14 @@ int stack_Dtor (struct Stack *stk)
 
 int stack_push (struct Stack *stk, double elem)
 {
-    if((stk -> size_stk) >= (stk -> capacity_stk))
+    if((stk->size_stk) >= (stk->capacity_stk))
     {
         stack_resize (stk, stk_increase);
     }
 
-    *((stk -> data_stk) + (stk -> size_stk)) = elem;
+    *((stk->buffer_stk) + (stk->size_stk)) = elem;
 
-    (stk -> size_stk)++;
+    (stk->size_stk)++;
 
     return SUCCESS;
 }
@@ -90,16 +107,16 @@ int stack_resize (struct Stack *stk, int opt_resize)
 {
     if (opt_resize == stk_increase)
     {
-        (stk -> capacity_stk)*=2;
+        stk->capacity_stk*=2;
 
-        (stk -> data_stk) = (double*) realloc ( (stk -> data_stk), (stk -> capacity_stk)*sizeof (double));
+        stk->buffer_stk = (double*) realloc (stk->buffer_stk, stk->capacity_stk*sizeof (double));
     }
 
     if (opt_resize == stk_decrease)
     {
-        (stk -> capacity_stk)/=2;
+        stk->capacity_stk/=2;
 
-        (stk -> data_stk) = (double*) realloc ( (stk -> data_stk), (stk -> capacity_stk)*sizeof (double));
+        stk->buffer_stk = (double*) realloc (stk->buffer_stk, stk->capacity_stk*sizeof (double));
     }
 
     return SUCCESS;
@@ -107,18 +124,52 @@ int stack_resize (struct Stack *stk, int opt_resize)
 
 //-----------------------------------------------------------------------------
 
-int stack_pop (struct Stack *stk)
+double stack_pop (struct Stack *stk)
 {
-    *((stk -> data_stk) + (stk -> size_stk)) = 0;
+    double elem_del = stk->buffer_stk[stk->size_stk];
 
-    (stk -> size_stk)--;
+    stk->size_stk--;
 
-    if((stk -> size_stk) <= ((stk -> capacity_stk) / 4))
+    if(stk->size_stk <= stk->capacity_stk / 4)
     {
         stack_resize (stk, stk_decrease);
     }
 
+    return elem_del;
+}
+
+//-----------------------------------------------------------------------------
+
+int stack_dmp    (struct Stack *stk)
+{
+    printf ("%s[%p](ok) at %s    \n"
+            "{                      \n"
+            "    size_stk     = %d  \n"
+            "    capacity_stk = %d  \n"
+            "                       \n"
+            "    buffer_stk[%p]       \n"
+            "    {                  \n",
+            stk->name_stk,
+            stk,
+            /*stk->cur_status_stk,*/
+            __FILE__,
+            stk->capacity_stk,
+            stk->size_stk,
+            stk->buffer_stk);
+
+    for(int pos = 0; pos < stk->capacity_stk; pos++)
+    {
+        printf (
+
+        "          *[%d] = %lg\n",
+        /*cur_status_elem*/ pos, stk->buffer_stk[pos]);
+    }
+
+    printf ("    }                  \n"
+            "}                      \n");
+
     return SUCCESS;
 }
 
 //-----------------------------------------------------------------------------
+
